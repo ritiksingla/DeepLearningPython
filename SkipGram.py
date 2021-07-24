6e91a610018679e292f38263f933c7161805a637
ritiksingla <ritiksingla28@gmail.com>
2021-07-24 22:57:12 +0530

    negative sampling added with skip-gram

--- SkipGram.py
+++ SkipGram.py
@@ -175,3 +175,145 @@ fig, ax = plt.subplots(figsize=(16, 16))
 for idx in range(viz_words):
     plt.scatter(*embed_tsne[idx, :], color="steelblue")
     plt.annotate(int_to_vocab[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha=0.7)
+
+# Negative Sampling
+
+class SkipGramNeg(nn.Module):
+    def __init__(self, n_vocab, n_embed, noise_dist=None):
+        super().__init__()
+        
+        self.n_vocab = n_vocab
+        self.n_embed = n_embed
+        self.noise_dist = noise_dist
+        
+        # define embedding layers for input and output words
+        self.in_embed = nn.Embedding(n_vocab, n_embed)
+        self.out_embed = nn.Embedding(n_vocab, n_embed)
+        
+        # Initialize embedding tables with uniform distribution
+        # I believe this helps with convergence
+        self.in_embed.weight.data.uniform_(-1, 1)
+        self.out_embed.weight.data.uniform_(-1, 1)
+        
+    def forward_input(self, input_words):
+        input_vectors = self.in_embed(input_words)
+        return input_vectors
+    
+    def forward_output(self, output_words):
+        output_vectors = self.out_embed(output_words)
+        return output_vectors
+    
+    def forward_noise(self, batch_size, n_samples):
+        """ Generate noise vectors with shape (batch_size, n_samples, n_embed)"""
+        if self.noise_dist is None:
+            # Sample words uniformly
+            noise_dist = torch.ones(self.n_vocab)
+        else:
+            noise_dist = self.noise_dist
+            
+        # Sample words from our noise distribution
+        noise_words = torch.multinomial(noise_dist,
+                                        batch_size * n_samples,
+                                        replacement=True)
+        
+        device = "cuda" if model.out_embed.weight.is_cuda else "cpu"
+        noise_words = noise_words.to(device)
+        
+        noise_vectors = self.out_embed(noise_words).view(batch_size, n_samples, self.n_embed)
+        
+        return noise_vectors
+
+class NegativeSamplingLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input_vectors, output_vectors, noise_vectors):
+        
+        batch_size, embed_size = input_vectors.shape
+        
+        # Input vectors should be a batch of column vectors
+        input_vectors = input_vectors.view(batch_size, embed_size, 1)
+        
+        # Output vectors should be a batch of row vectors
+        output_vectors = output_vectors.view(batch_size, 1, embed_size)
+        
+        # bmm = batch matrix multiplication
+        # correct log-sigmoid loss
+        out_loss = torch.bmm(output_vectors, input_vectors).sigmoid().log()
+        out_loss = out_loss.squeeze()
+        
+        # incorrect log-sigmoid loss
+        noise_loss = torch.bmm(noise_vectors.neg(), input_vectors).sigmoid().log()
+        noise_loss = noise_loss.squeeze().sum(1)  # sum the losses over the sample of noise vectors
+
+        # negate and sum correct and noisy log-sigmoid losses
+        # return average batch loss
+        return -(out_loss + noise_loss).mean()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Get our noise distribution
+# Using word frequencies calculated earlier in the notebook
+word_freqs = np.array(sorted(freqs.values(), reverse=True))
+unigram_dist = word_freqs/word_freqs.sum()
+noise_dist = torch.from_numpy(unigram_dist**(0.75)/np.sum(unigram_dist**(0.75)))
+
+# instantiating the model
+embedding_dim = 300
+model = SkipGramNeg(len(vocab_to_int), embedding_dim, noise_dist=noise_dist).to(device)
+
+# using the loss that we defined
+criterion = NegativeSamplingLoss() 
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+
+print_every = 1500
+steps = 0
+epochs = 5
+
+# train for some number of epochs
+for e in range(epochs):
+    
+    # get our input, target batches
+    for input_words, target_words in get_batches(train_words, 512):
+        steps += 1
+        inputs, targets = torch.LongTensor(input_words), torch.LongTensor(target_words)
+        inputs, targets = inputs.to(device), targets.to(device)
+        
+        # input, output, and noise vectors
+        input_vectors = model.forward_input(inputs)
+        output_vectors = model.forward_output(targets)
+        noise_vectors = model.forward_noise(inputs.shape[0], 5)
+
+        # negative sampling loss
+        loss = criterion(input_vectors, output_vectors, noise_vectors)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # loss stats
+        if steps % print_every == 0:
+            print("Epoch: {}/{}".format(e+1, epochs))
+            print("Loss: ", loss.item()) # avg batch loss at this point in training
+            valid_examples, valid_similarities = cosine_similarity(model.in_embed, device=device)
+            _, closest_idxs = valid_similarities.topk(6)
+
+            valid_examples, closest_idxs = valid_examples.to('cpu'), closest_idxs.to('cpu')
+            for ii, valid_idx in enumerate(valid_examples):
+                closest_words = [int_to_vocab[idx.item()] for idx in closest_idxs[ii]][1:]
+                print(int_to_vocab[valid_idx.item()] + " | " + ', '.join(closest_words))
+            print("...\n")
+
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+embeddings = model.in_embed.weight.to("cpu").data.numpy()
+viz_words = 380
+tsne = TSNE()
+embed_tsne = tsne.fit_transform(embeddings[:viz_words, :])
+
+fig, ax = plt.subplots(figsize = (16, 16))
+for idx in range(viz_words):
+    plt.scatter(*embed_tsne[idx,:], color = "steelblue")
+    plt.annotate(int_to_vocab[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha = 0.7)
